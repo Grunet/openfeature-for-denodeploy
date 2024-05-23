688a97aa026695143237ea916dc73a550e5ca9ae
Grunet <sadikshahidain@gmail.com>
2024-05-23 22:05:23 +0000

    start with dependencies

---
+++ src/deps.ts
@@ -0,0 +1,12 @@
+export {
+  AnyProviderEvent,
+  EvaluationContext,
+  Hook,
+  JsonValue,
+  Logger,
+  Provider,
+  ProviderEventEmitter,
+  ResolutionDetails,
+} from "npm:@openfeature/server-sdk@1.14.0"; //Copied from https://github.com/open-feature/js-sdk/blob/main/packages/server/README.md#develop-a-provider.
+
+export { FlagdCore } from "npm:@openfeature/flagd-core@0.2.2";
