ee87a9f0d045e23b327c4536de526bd1e64ea4e2
Grunet <sadikshahidain@gmail.com>
2024-05-23 14:02:49 -0500

    scaffold github codespaces (#1)

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Adds 2 numbers together
+ * @param num1 the first number
+ * @param num2 the second number
+ * @returns the sum of the 2 numbers
+ */
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+export { sum }
