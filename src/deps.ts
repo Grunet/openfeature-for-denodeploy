da666f4d8f153e93a62e103242b25f8d6cfcad8e
Grunet <sadikshahidain@gmail.com>
2024-05-23 22:27:12 +0000

    fix dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,12 +1,16 @@
-export {
+//Copied and adjusted from https://github.com/open-feature/js-sdk/blob/main/packages/server/README.md#develop-a-provider.
+
+export type {
   AnyProviderEvent,
   EvaluationContext,
-  Hook,
+  // Hook,
   JsonValue,
   Logger,
-  Provider,
+  // Provider,
   ProviderEventEmitter,
   ResolutionDetails,
-} from "npm:@openfeature/server-sdk@1.14.0"; //Copied from https://github.com/open-feature/js-sdk/blob/main/packages/server/README.md#develop-a-provider.
+} from "npm:@openfeature/core@1.2.0";
+
+export type { Hook, Provider } from "npm:@openfeature/server-sdk@1.14.0";
 
 export { FlagdCore } from "npm:@openfeature/flagd-core@0.2.2";
